d1f0634ef4bd24d8a246b59ac243d8eaa29eca82
Techcps <104138529+Techcps@users.noreply.github.com>
2024-03-15 17:51:47 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Get Started with Cloud Storage: Challenge Lab/techcpsarc111.sh
+++ Get Started with Cloud Storage: Challenge Lab/techcpsarc111.sh
@@ -1,13 +1,11 @@
 
-gcloud config set compute/region $REGION
-
 gsutil mb -c coldline gs://$DEVSHELL_PROJECT_ID-bucket
 
-gsutil mb gs://$DEVSHELL_PROJECT_ID-gcs-bucket
+# gsutil mb gs://$DEVSHELL_PROJECT_ID-gcs-bucket
 
 gsutil cp -a public-read sample.txt gs://$DEVSHELL_PROJECT_ID-gcs-bucket/sample.txt
 echo "This is an example of editing the file content for cloud storage object" | gsutil -h "Content-Type:text/plain" -h "Cache-Control:public, max-age=0" cp - gs://$DEVSHELL_PROJECT_ID-gcs-bucket/sample.txt
 
-gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket-ops
+# gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket-ops
 
 gsutil defstorageclass set archive gs://$DEVSHELL_PROJECT_ID-bucket-ops
