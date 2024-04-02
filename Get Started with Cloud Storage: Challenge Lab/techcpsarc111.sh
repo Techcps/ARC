9c924298b2b299fecb19283f4cffbeaf897cac1b
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-02 18:21:01 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Get Started with Cloud Storage: Challenge Lab/techcpsarc111.sh
+++ Get Started with Cloud Storage: Challenge Lab/techcpsarc111.sh
@@ -1,8 +1,8 @@
 
-gsutil mb gs://$DEVSHELL_PROJECT_ID-bucket
+gsutil mb gs://$BUCKET-bucket
 
-gsutil retention set 30s gs://$DEVSHELL_PROJECT_ID-gcs-bucket
+gsutil retention set 30s gs://$BUCKET2-gcs-bucket
 
 echo "Hello, CP Cloud Storage!" > hello.txt
-gsutil cp hello.txt gs://$DEVSHELL_PROJECT_ID-bucket-ops/
+gsutil cp hello.txt gs://$BUCKET3-bucket-ops/
 
