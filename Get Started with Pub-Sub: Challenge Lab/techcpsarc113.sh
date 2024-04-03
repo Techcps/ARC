be92db5054f974a654f51af27d47193b10ed2cb2
Techcps <104138529+Techcps@users.noreply.github.com>
2024-04-03 18:15:48 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

--- Get Started with Pub-Sub: Challenge Lab/techcpsarc113.sh
+++ Get Started with Pub-Sub: Challenge Lab/techcpsarc113.sh
@@ -1,11 +1,70 @@
+# Get the form number from the from input
+read -p "Enter the Form number (1, 2, or 3): " form_number
 
-gcloud services enable cloudscheduler.googleapis.com --project=$DEVSHELL_PROJECT_ID
+# Function to run Form 1
+run_form_1() {
+    gcloud services enable cloudscheduler.googleapis.com --project=$DEVSHELL_PROJECT_ID
 
-gcloud pubsub topics create cloud-pubsub-topic
+    gcloud pubsub topics create cloud-pubsub-topic
 
-gcloud pubsub subscriptions create cloud-pubsub-subscription --topic=cloud-pubsub-topic
+    gcloud pubsub subscriptions create 'cloud-pubsub-subscription' --topic=cloud-pubsub-topic
 
-gcloud scheduler jobs create pubsub cron-scheduler-job --schedule="* * * * *" --topic=cron-job-pubsub-topic --message-body="Hello World!" --time-zone="America/Los_Angeles" --location=$REGION
+    gcloud scheduler jobs create pubsub cron-scheduler-job --schedule="* * * * *" --topic=cron-job-pubsub-topic --message-body="Hello World!" --location=$REGION
 
-gcloud pubsub subscriptions pull cron-job-pubsub-subscription --limit 5
+    gcloud pubsub subscriptions pull cron-job-pubsub-subscription --limit 5
+}
 
+# Function to run Form 2
+run_form_2() {
+    gcloud beta pubsub schemas create city-temp-schema \
+        --type=avro \
+        --definition='{
+            "type": "record",
+            "name": "Avro",
+            "fields": [
+                {
+                    "name": "city",
+                    "type": "string"
+                },
+                {
+                    "name": "temperature",
+                    "type": "double"
+                },
+                {
+                    "name": "pressure",
+                    "type": "int"
+                },
+                {
+                    "name": "time_position",
+                    "type": "string"
+                }
+            ]
+        }'
+
+    gcloud pubsub topics create temp-topic --message-encoding=JSON --message-storage-policy-allowed-regions=$REGION --schema=projects/$DEVSHELL_PROJECT_ID/schemas/temperature-schema
+}
+
+# Function to run Form 3
+run_form_3() {
+    gcloud pubsub snapshots create pubsub-snapshot --subscription=gcloud-pubsub-subscription
+
+    gcloud pubsub lite-reservations create pubsub-lite-reservation --location=$REGION --throughput-capacity=2
+
+    gcloud services enable cloudscheduler.googleapis.com --project=$DEVSHELL_PROJECT_ID
+
+    gcloud pubsub lite-topics create cloud-pubsub-topic-lite --location=$REGION --partitions=1 --per-partition-bytes=30GiB --throughput-reservation=demo-reservation
+
+    gcloud pubsub lite-subscriptions create cloud-pubsub-subscription-lite --location=$REGION --topic=cloud-pubsub-topic-lite
+}
+
+# Run the function based on the selected form number
+case $form_number in
+    1) 
+        run_form_1 || run_form_2 ;;
+    2) 
+        run_form_2 || run_form_3 ;;
+    3) 
+        run_form_3 ;;
+    *) 
+        echo "CP Invalid form number. Please enter 1, 2, or 3." ;;
+esac
