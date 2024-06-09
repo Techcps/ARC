59bdc0633731f74fb641f78742e51f6399e7ff23
Techcps <104138529+Techcps@users.noreply.github.com>
2024-06-10 00:02:09 +0530

    Please like share & subscribe to Techcps (https://www.youtube.com/@techcps)

---
+++ Cloud Speech API 3 Ways: Challenge Lab/techcps.sh
@@ -0,0 +1,122 @@
+# ARC132!
+
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+source venv/bin/activate
+
+
+cat > synthesize-text.json <<EOF_CP
+
+{
+    'input':{
+        'text':'Cloud Text-to-Speech API allows developers to include
+           natural-sounding, synthetic human speech as playable audio in
+           their applications. The Text-to-Speech API converts text or
+           Speech Synthesis Markup Language (SSML) input into audio data
+           like MP3 or LINEAR16 (the encoding used in WAV files).'
+    },
+    'voice':{
+        'languageCode':'en-gb',
+        'name':'en-GB-Standard-A',
+        'ssmlGender':'FEMALE'
+    },
+    'audioConfig':{
+        'audioEncoding':'MP3'
+    }
+}
+
+EOF_CP
+
+
+curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d @synthesize-text.json "https://texttospeech.googleapis.com/v1/text:synthesize" \
+  > $file_CP2
+
+
+
+cat > tts_decode.py <<EOF_CP
+import argparse
+from base64 import decodebytes
+import json
+"""
+Usage:
+        python tts_decode.py --input "synthesize-text.txt" \
+        --output "synthesize-text-audio.mp3"
+"""
+def decode_tts_output(input_file, output_file):
+    """ Decode output from Cloud Text-to-Speech.
+    input_file: the response from Cloud Text-to-Speech
+    output_file: the name of the audio file to create
+    """
+    with open(input_file) as input:
+        response = json.load(input)
+        audio_data = response['audioContent']
+        with open(output_file, "wb") as new_file:
+            new_file.write(decodebytes(audio_data.encode('utf-8')))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Decode output from Cloud Text-to-Speech",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--input',
+                       help='The response from the Text-to-Speech API.',
+                       required=True)
+    parser.add_argument('--output',
+                       help='The name of the audio file to create',
+                       required=True)
+    args = parser.parse_args()
+    decode_tts_output(args.input, args.output)
+EOF_CP
+
+python tts_decode.py --input "$request_CP2" --output "synthesize-text-audio.mp3"
+
+
+
+
+
+audio_uri="gs://cloud-samples-data/speech/corbeau_renard.flac"
+
+
+
+cat > "$request_CP2" <<EOF_CP
+{
+  "config": {
+    "encoding": "FLAC",
+    "sampleRateHertz": 44100,
+    "languageCode": "fr-FR"
+  },
+  "audio": {
+    "uri": "$audio_uri"
+  }
+}
+EOF_CP
+
+
+curl -s -X POST -H "Content-Type: application/json" \
+    --data-binary @"$request_CP2" \
+    "https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" \
+    -o "$response_CP3"
+
+
+
+sudo apt-get update
+sudo apt-get install -y jq
+
+
+curl "https://translation.googleapis.com/language/translate/v2?target=en&key=${API_KEY}&q=${sentence_CP4}" > $file_CP4
+
+
+
+decoded_sentence=$(python -c "import urllib.parse; print(urllib.parse.unquote('$sentence_CP5'))")
+
+
+curl -s -X POST \
+  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+  -H "Content-Type: application/json; charset=utf-8" \
+  -d "{\"q\": [\"$decoded_sentence\"]}" \
+  "https://translation.googleapis.com/language/translate/v2/detect?key=${API_KEY}" \
+  -o "$file_CP5"
+
+
+
