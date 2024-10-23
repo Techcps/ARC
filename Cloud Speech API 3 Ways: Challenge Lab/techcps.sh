186c96760631fe3f706456d580adbbff022a4d64
Techcps <104138529+Techcps@users.noreply.github.com>
2024-10-23 16:57:26 +0530

    Please like share & subscribe to Techcps

--- Cloud Speech API 3 Ways: Challenge Lab/techcps.sh
+++ Cloud Speech API 3 Ways: Challenge Lab/techcps.sh
@@ -1,64 +1,76 @@
 # ARC132!
 
 
-gcloud auth list
-
-export ZONE=$(gcloud compute instances list lab-vm --format 'csv[no-heading](zone)')
-
-export PROJECT_ID=$(gcloud config get-value project)
-
-export PROJECT_ID=$DEVSHELL_PROJECT_ID
-
-echo "Please export the values correctly."
-
-
-# Enter the variable name correctly
-read -p "Enter API_KEY: " API_KEY
-read -p "Enter file_cp2: " file_cp2
-read -p "Enter request_cp3: " request_cp3
-read -p "Enter response_cp3: " response_cp3
-read -p "Enter sentence_cp4: " sentence_cp4
-read -p "Enter file_cp4: " file_cp4
-read -p "Enter sentence_cp5: " sentence_cp5
-read -p "Enter file_cp5: " file_cp5
-
-
-cat > cp_disk.sh <<'EOF_CP'
-
-cp="gs://cloud-samples-data/speech/corbeau_renard.flac"
-
 export PROJECT_ID=$(gcloud config get-value project)
 
 source venv/bin/activate
 
-
 cat > synthesize-text.json <<EOF
 
 {
-'input':{
-   'text':'Cloud Text-to-Speech API allows developers to include
-      natural-sounding, synthetic human speech as playable audio in
-      their applications. The Text-to-Speech API converts text or
-      Speech Synthesis Markup Language (SSML) input into audio data
-      like MP3 or LINEAR16 (the encoding used in WAV files).'
-},
-'voice':{
-   'languageCode':'en-gb',
-   'name':'en-GB-Standard-A',
-   'ssmlGender':'FEMALE'
-},
-'audioConfig':{
-   'audioEncoding':'MP3'
+    'input':{
+        'text':'Cloud Text-to-Speech API allows developers to include
+           natural-sounding, synthetic human speech as playable audio in
+           their applications. The Text-to-Speech API converts text or
+           Speech Synthesis Markup Language (SSML) input into audio data
+           like MP3 or LINEAR16 (the encoding used in WAV files).'
+    },
+    'voice':{
+        'languageCode':'en-gb',
+        'name':'en-GB-Standard-A',
+        'ssmlGender':'FEMALE'
+    },
+    'audioConfig':{
+        'audioEncoding':'MP3'
+    }
 }
-}
-
 EOF
 
 
 curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
 -H "Content-Type: application/json; charset=utf-8" \
 -d @synthesize-text.json "https://texttospeech.googleapis.com/v1/text:synthesize" \
-> $file_cp2
+> "$file_cp2"
+
+
+
+cat > tts_decode.py <<EOF
+import argparse
+from base64 import decodebytes
+import json
+"""
+Usage:
+        python tts_decode.py --input "synthesize-text.txt" \
+        --output "synthesize-text-audio.mp3"
+"""
+def decode_tts_output(input_file, output_file):
+    """ Decode output from Cloud Text-to-Speech.
+    input_file: the response from Cloud Text-to-Speech
+    output_file: the name of the audio file to create
+    """
+    with open(input_file) as input:
+        response = json.load(input)
+        audio_data = response['audioContent']
+        with open(output_file, "wb") as new_file:
+            new_file.write(decodebytes(audio_data.encode('utf-8')))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Decode output from Cloud Text-to-Speech",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--input',
+                       help='The response from the Text-to-Speech API.',
+                       required=True)
+    parser.add_argument('--output',
+                       help='The name of the audio file to create',
+                       required=True)
+    args = parser.parse_args()
+    decode_tts_output(args.input, args.output)
+EOF
+
+python tts_decode.py --input "$file_cp2" --output "synthesize-text-audio.mp3"
+
+
+audio_uri="gs://cloud-samples-data/speech/corbeau_renard.flac"
 
 
 cat > "$request_cp3" <<EOF
@@ -69,11 +81,10 @@ cat > "$request_cp3" <<EOF
 "languageCode": "fr-FR"
 },
 "audio": {
-"uri": "$cp"
+"uri": "$audio_uri"
 }
 }
 EOF
-
 
 curl -s -X POST -H "Content-Type: application/json" \
 --data-binary @"$request_cp3" \
@@ -95,11 +106,4 @@ curl -s -X POST \
 -d "{\"q\": [\"$sentence_cp5\"]}" \
 "https://translation.googleapis.com/language/translate/v2/detect?key=${API_KEY}" \
 -o "$file_cp5"
-EOF_CP
-
-
-gcloud compute scp cp_disk.sh lab-vm:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
-
-gcloud compute ssh lab-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/cp_disk.sh"
-
 
