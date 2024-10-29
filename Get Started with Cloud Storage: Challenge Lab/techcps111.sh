d0a2aa237c8d3fe7083f744246513018846ff175
Techcps <104138529+Techcps@users.noreply.github.com>
2024-10-29 12:46:32 +0530

    Please like share & subscribe to Techcps

--- Get Started with Cloud Storage: Challenge Lab/techcps111.sh
+++ Get Started with Cloud Storage: Challenge Lab/techcps111.sh
@@ -1,4 +1,5 @@
 
+
 # Get the form number from the from input
 read -p "Enter the Form number (1, 2, or 3): " form_number
 
@@ -6,46 +7,54 @@ read -p "Enter the Form number (1, 2, or 3): " form_number
 cp_form_1() {
 export BUCKET="$(gcloud config get-value project)"		
 
-gsutil mb -p $BUCKET gs://$BUCKET-bucket
+gsutil mb -c coldline gs://$BUCKET_1
 
-gsutil retention set 30s gs://$BUCKET-gcs-bucket
+gsutil retention set 30s gs://$BUCKET_2
 
 echo "please subscibe to techcps" > sample.txt
 
-gsutil cp sample.txt gs://$BUCKET-bucket-ops/
+gsutil cp sample.txt gs://$BUCKET_3/
 }
 
 # Function to run Form 2
 cp_form_2() {
 export BUCKET="$(gcloud config get-value project)"		
 
-gsutil mb -c nearline gs://$BUCKET-bucket
+gsutil mb -c nearline gs://$BUCKET_1
 
-gcloud alpha storage buckets update gs://$BUCKET-gcs-bucket --no-uniform-bucket-level-access
+gcloud alpha storage buckets update gs://$BUCKET_2 --no-uniform-bucket-level-access
 
-gsutil acl ch -u $USER_EMAIL:OWNER gs://$BUCKET-gcs-bucket
+gsutil acl ch -u $USER_EMAIL:OWNER gs://$BUCKET_2
 
-gsutil rm gs://$BUCKET-gcs-bucket/sample.txt
+gsutil rm gs://$BUCKET_2/sample.txt
 
 echo "please subscibe to techcps" > sample.txt
 
-gsutil cp sample.txt gs://$BUCKET-gcs-bucket
+gsutil cp sample.txt gs://$BUCKET_2
 
-gsutil acl ch -u allUsers:R gs://$BUCKET-gcs-bucket/sample.txt
+gsutil acl ch -u allUsers:R gs://$BUCKET_2
 
-gcloud storage buckets update gs://$BUCKET-bucket-ops --update-labels=key=value
+gcloud storage buckets update gs://$BUCKET_3 --update-labels=key=value
 }
 
 # Function to run Form 3
 cp_form_3() {
 export BUCKET="$(gcloud config get-value project)"		
 
-gsutil mb -c coldline gs://$BUCKET-bucket
+gsutil mb -c coldline gs://$BUCKET_1
 
-echo "This is an example of editing the file content for cloud storage object" | gsutil cp - gs://$BUCKET-gcs-bucket/sample.txt
+echo "This is an example of editing the file content for cloud storage object" | gsutil cp - gs://$BUCKET_2
 
-gsutil defstorageclass set ARCHIVE gs://$BUCKET-bucket-ops
+gsutil defstorageclass set ARCHIVE gs://$BUCKET_3
 }
+
+
+echo "Please set the below values correctly"
+
+# Export the buckets name correctly
+read -p "Enter the BUCKET_1: " BUCKET_1
+read -p "Enter the BUCKET_2: " BUCKET_2
+read -p "Enter the BUCKET_3: " BUCKET_3
 
 # Run the function based on the selected form number
 case $form_number in
