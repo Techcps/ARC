6fa06b86c7a512ce1d2dcec32262d177d2dd107c
Techcps <104138529+Techcps@users.noreply.github.com>
2024-12-05 19:40:37 +0530

    Create techcps129.sh

---
+++ Secure BigLake Data: Challenge Lab/techcps129.sh
@@ -0,0 +1,160 @@
+
+
+# Set text styles
+YELLOW=$(tput setaf 3)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+echo "Please set the below values correctly"
+read -p "Enter the USER_2: " USER_2
+
+bq mk --dataset online_shop
+
+bq mk --connection --location=US --connection_type=CLOUD_RESOURCE user_data_connection
+
+SERVICE_ACCOUNT=$(bq show --format=json --connection $DEVSHELL_PROJECT_ID.US.user_data_connection | jq -r '.cloudResource.serviceAccountId')
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=serviceAccount:$SERVICE_ACCOUNT \
+  --role=roles/storage.objectViewer
+
+bq mkdef \
+--autodetect \
+--connection_id=$DEVSHELL_PROJECT_ID.US.user_data_connection \
+--source_format=CSV \
+"gs://$DEVSHELL_PROJECT_ID-bucket/user-online-sessions.csv" > /tmp/tabledef.json
+
+bq mk --external_table_definition=/tmp/tabledef.json \
+--project_id=$DEVSHELL_PROJECT_ID \
+online_shop.user_online_sessions
+
+TAXONOMY_NAME=$(gcloud data-catalog taxonomies list \
+  --location=us \
+  --project=$DEVSHELL_PROJECT_ID \
+  --format="value(displayName)" \
+  --limit=1)
+
+TAXONOMY_ID=$(gcloud data-catalog taxonomies list \
+  --location=us \
+  --format="value(name)" \
+  --filter="displayName=$TAXONOMY_NAME" | awk -F'/' '{print $6}')
+
+POLICY_TAG=$(gcloud data-catalog taxonomies policy-tags list \
+  --location=us \
+  --taxonomy=$TAXONOMY_ID \
+  --format="value(name)" \
+  --limit=1)
+
+
+cat > schema.json << EOM
+[
+  {
+    "mode": "NULLABLE",
+    "name": "ad_event_id",
+    "type": "INTEGER"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "user_id",
+    "type": "INTEGER"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "uri",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "traffic_source",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "zip",
+    "policyTags": {
+      "names": [
+        "$POLICY_TAG"
+      ]
+    },
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "event_type",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "state",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "country",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "city",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "latitude",
+    "policyTags": {
+      "names": [
+        "$POLICY_TAG"
+      ]
+    },
+    "type": "FLOAT"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "created_at",
+    "type": "TIMESTAMP"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "ip_address",
+    "policyTags": {
+      "names": [
+        "$POLICY_TAG"
+      ]
+    },
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "session_id",
+    "type": "STRING"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "longitude",
+    "policyTags": {
+      "names": [
+        "$POLICY_TAG"
+      ]
+    },
+    "type": "FLOAT"
+  },
+  {
+    "mode": "NULLABLE",
+    "name": "id",
+    "type": "INTEGER"
+  }
+]
+EOM
+
+
+bq update --schema schema.json $DEVSHELL_PROJECT_ID:online_shop.user_online_sessions
+
+bq query --use_legacy_sql=false --format=csv \
+"SELECT * EXCEPT(zip, latitude, ip_address, longitude) 
+FROM \`${DEVSHELL_PROJECT_ID}.online_shop.user_online_sessions\`"
+
+gcloud projects remove-iam-policy-binding ${DEVSHELL_PROJECT_ID} \
+  --member="user:$USER_2" \
+  --role="roles/storage.objectViewer"
+
+
